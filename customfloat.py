b6724ac8ce5a935e0e369b6c095e5cf315a562bf
Brett Tuttle <tuttle.brett@gmail.com>
2015-02-17 17:56:05 -0500

    Began implementing a conversion to decimal, but it is failing initial tests by not rounding off when it should.

--- customfloat.py
+++ customfloat.py
@@ -1,24 +1,51 @@
 class CustomFloat():
-    def __init__(self, specification):
+    def __init__(self, specification, **kwargs):
         """Creates a custom floating point number
 
         Arguments:
-        specification -- the floating point specification that this number complies with
+        specification -- The floating point specification that this number 
+            complies with
+
+        Key Word Arguments:
+        sign -- The bit value of the sign bit if the number has a sign bit. 0 
+            for positive, 1 for negative (Default 0).
+        exponent -- The bit value of the exponent if the number has exponent 
+            bits. Inputs must be between 0 and 2^n - 1 where n is the number of
+            exponent bits (Default: A value that when adjusted for the bias will
+            result in 0)
+        mantissa -- The bit value of the mantissa. Do not include a leading 1
+            for normalized numbers (Default 0).
         """
         self.specification = specification
         
-        if specification.sign:
-            self.sign = 0b0
+        if specification.sign and kwargs['sign']:
+            self.sign = kwargs['sign']
+        elif specification.sign:
+            self.sign = 0
 
-        if specification.exponent > 0:
-            self.exponent = 0b0
+        if specification.exponent > 0 and kwargs['exponent']:
+            self.exponent = kwargs['exponent']
+        elif specification.exponent > 0:
+            self.exponent = specification.bias
 
-        self.mantissa = 0b0
+        if kwargs['mantissa']:
+            self.mantissa = kwargs['mantissa']
+        else:
+            self.mantissa = 0
+   
+    def getValue(self):
+        val = (1 + self.mantissa * 2 ** (-1 * self.specification.mantissa)) * \
+                2 ** (self.exponent - self.specification.bias)
+        if self.sign and self.sign == 1:
+            val *= -1
+
+        return val
 
 class FloatSpecification():
     __slots__ = ('sign', 'exponent', 'mantissa', 'special_values', 'bias')
 
     def __init__(self, exponent, mantissa, sign=True, special_values=True):
+        #TODO: Account for denormalized special values
         """Creates a floating point specification
 
         Arguments:
@@ -27,7 +54,8 @@ class FloatSpecification():
         
         Keyword Arguments:
         sign -- Determines if the number has a sign bit (default=True)
-        special_values -- Determines if infinite and nan values can be represented (default=True)
+        special_values -- Determines if infinite and nan values can be 
+            represented (default=True)
         """
         self.sign = sign
         self.exponent = exponent
@@ -64,6 +92,12 @@ class FloatSpecification():
     def getMinValue(self):
         if self.sign:
             return -1 * self.getMaxValue()
+        else:
+            return 0
+
+    def getMinFraction(self):
+        if self.exponent > 0:
+            return
         else:
             return 0
 
--- customfloattest.py
+++ customfloattest.py
@@ -1,62 +1,72 @@
 import unittest
-import customfloat
+from customfloat import *
 
-class MaxValue(unittest.TestCase):
+class SpecMaxValue(unittest.TestCase):
 
     def testIEEESingle(self):
-        self.assertEqual(customfloat.IEEE_SINGLE_PRECISION.getMaxValue(), 340282346638528859811704183484516925440, "Incorrect IEEE Single Precision max value")
+        self.assertEqual(IEEE_SINGLE_PRECISION.getMaxValue(), 340282346638528859811704183484516925440, "Incorrect IEEE Single Precision max value")
 
     def testIEEEDouble(self):
-        self.assertEqual(customfloat.IEEE_DOUBLE_PRECISION.getMaxValue(),
+        self.assertEqual(IEEE_DOUBLE_PRECISION.getMaxValue(),
                 179769313486231570814527423731704356798070567525844996598917476803157260780028538760589558632766878171540458953514382464234321326889464182768467546703537516986049910576551282076245490090389328944075868508455133942304583236903222948165808559332123348274797826204144723168738177180919299881250404026184124858368, "Incorrect IEEE Double Precision max value")
 
     def testCustomSpecStandard(self):
-        custom_spec = customfloat.FloatSpecification(3, 6)
+        custom_spec = FloatSpecification(3, 6)
         self.assertEqual(custom_spec.getMaxValue(), 15.875)
 
     def testCustomSpecNoSign(self):
-        custom_spec = customfloat.FloatSpecification(3, 6, sign=False)
+        custom_spec = FloatSpecification(3, 6, sign=False)
         self.assertEqual(custom_spec.getMaxValue(), 15.875)
 
     def testCustomSpecNoSpecialValues(self):
-        custom_spec = customfloat.FloatSpecification(3, 6, special_values=False)
+        custom_spec = FloatSpecification(3, 6, special_values=False)
         self.assertEqual(custom_spec.getMaxValue(), 31.75)
 
     def testCustomSpecNoExponent(self):
-        custom_spec = customfloat.FloatSpecification(0, 6)
+        custom_spec = FloatSpecification(0, 6)
         self.assertEqual(custom_spec.getMaxValue(), 63)
 
     def testCustomSpecMantissaOnly(self):
-        custom_spec = customfloat.FloatSpecification(0, 6, sign=False, special_values=False)
+        custom_spec = FloatSpecification(0, 6, sign=False, special_values=False)
         self.assertEqual(custom_spec.getMaxValue(), 63)
 
-class MinValue(unittest.TestCase):
+class SpecMinValue(unittest.TestCase):
 
     def testIEEESingle(self):
-        self.assertEqual(customfloat.IEEE_SINGLE_PRECISION.getMinValue(), -340282346638528859811704183484516925440, "Incorrect IEEE Single Precision min value")
+        self.assertEqual(IEEE_SINGLE_PRECISION.getMinValue(), -340282346638528859811704183484516925440, "Incorrect IEEE Single Precision min value")
 
     def testIEEEDouble(self):
-        self.assertEqual(customfloat.IEEE_DOUBLE_PRECISION.getMinValue(), -179769313486231570814527423731704356798070567525844996598917476803157260780028538760589558632766878171540458953514382464234321326889464182768467546703537516986049910576551282076245490090389328944075868508455133942304583236903222948165808559332123348274797826204144723168738177180919299881250404026184124858368, "Incorrect IEEE Double Precision min value")
+        self.assertEqual(IEEE_DOUBLE_PRECISION.getMinValue(), -179769313486231570814527423731704356798070567525844996598917476803157260780028538760589558632766878171540458953514382464234321326889464182768467546703537516986049910576551282076245490090389328944075868508455133942304583236903222948165808559332123348274797826204144723168738177180919299881250404026184124858368, "Incorrect IEEE Double Precision min value")
 
     def testCustomSpecStandard(self):
-        custom_spec = customfloat.FloatSpecification(3, 6)
+        custom_spec = FloatSpecification(3, 6)
         self.assertEqual(custom_spec.getMinValue(), -15.875)
 
     def testCustomSpecNoSign(self):
-        custom_spec = customfloat.FloatSpecification(3, 6, sign=False)
+        custom_spec = FloatSpecification(3, 6, sign=False)
         self.assertEqual(custom_spec.getMinValue(), 0)
 
     def testCustomSpecNoSpecialValues(self):
-        custom_spec = customfloat.FloatSpecification(3, 6, special_values=False)
+        custom_spec = FloatSpecification(3, 6, special_values=False)
         self.assertEqual(custom_spec.getMinValue(), -31.75)
 
     def testCustomSpecNoExponent(self):
-        custom_spec = customfloat.FloatSpecification(0, 6)
+        custom_spec = FloatSpecification(0, 6)
         self.assertEqual(custom_spec.getMinValue(), -63)
 
     def testCustomSpecMantissaOnly(self):
-        custom_spec = customfloat.FloatSpecification(0, 6, sign=False, special_values=False)
+        custom_spec = FloatSpecification(0, 6, sign=False, special_values=False)
         self.assertEqual(custom_spec.getMinValue(), 0)
+
+class CustomFloatGetValue(unittest.TestCase):
+    pass
+    """
+TEST FAILING - This seems to be due to a rounding error. The value returned is correct for
+ a double precision number but it is giving too many digits for an unknown reason.
+    def testIEEESinglePositive(self):
+        num = CustomFloat(IEEE_SINGLE_PRECISION, sign=0, exponent=134, mantissa=4458529)
+        self.assertEqual(num.getValue(), 196.03175)
+    """
 
 if __name__ == "__main__":
     unittest.main()
